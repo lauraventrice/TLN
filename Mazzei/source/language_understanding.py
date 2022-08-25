e3b45156aafaf6e90e664a9ccca0022f083c3d3b
Laura <laura.ventrice@edu.unito.it>
2022-08-25 21:06:43 +0200

    added other comments

--- Mazzei/source/language_understanding.py
+++ Mazzei/source/language_understanding.py
@@ -1,3 +1,27 @@
 # correttezza sintattica uso di NLTK???????????
 # uso di stanza per il parser a dipendenze
 # funzione is_claim per individuare claim negazioni o frasi neutre --cercare lista di parole con accezione pos e ng
+
+#import language_tool_python as lt
+
+class LanguageUnderstanding: 
+
+    def check_sentence(self, sentence: str):
+        #uso di language tool per la correttezza sintattica
+        # se lo score è minore di 0.5 allora non è comprensibile, altrimenti si corregge per poi essere interpretata dalla parsificazione
+        pass
+    
+    def score_sentence(selft, sentence: str): 
+        #calcolo score 1-errori/totale parole PRENDERE DAL SITO
+        # usare spacy per splittare in sentence, oppure mi basta la funzione del sito?    
+        pass
+
+    def parsing_sentence(self, sentence: str, ingredients: list):
+        #uso di stanza per il parser a dipendenze     
+        #analizziamo l'albero per individuare i sotto alberi con noun chunks e dove viene detto un ingrediente
+
+        pass
+    
+    def is_claim(self, sentence: str):
+        #dobbiamo capire che fare..
+        pass
--- Mazzei/source/speech_recognition.py
+++ Mazzei/source/speech_recognition.py
@@ -1,3 +1,13 @@
 #Lettura frasi da terminale
 #Interpretazione frasi con uso della libreria SpeechRecognition
-# TROVARE DIZIONARIO DI LEMMI POSITIVI E NEGATIVI
+
+#import SpeechRecognition as sr
+
+class SpeechRecognizer: 
+
+    def read_from_terminal(self):
+        return input("Student: ")
+
+    def response_interpretation(self, response: str):
+        #qui uso di speech recognition
+        pass
